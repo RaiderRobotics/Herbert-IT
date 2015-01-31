bfc31739ae3f0df3e38f051eb2c963a53495add9
salamander2 <mk_gecko@yahoo.com>
2015-01-31 13:24:30 -0500

    Added camera things

--- src/org/raiderrobotics/Robot.java
+++ src/org/raiderrobotics/Robot.java
@@ -1,5 +1,13 @@
 package org.raiderrobotics;
 
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.DrawMode;
+import com.ni.vision.NIVision.Image;
+
+
+import com.ni.vision.NIVision.ShapeMode;
+
+
 //import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.*;
 
@@ -11,11 +19,14 @@ import edu.wpi.first.wpilibj.*;
  * directory.
  */
 public class Robot extends IterativeRobot {
-  
-  //constants
+
+	int session;
+	Image frame;
+
+	//constants
 	final static int ARCADE = 1;
 	final static int TANK = 2;
-	
+
 	//global variables
 	private int driveState = ARCADE;
 
@@ -26,13 +37,21 @@ public class Robot extends IterativeRobot {
 
 
 	/*This function is run when the robot is first started up and should be used for any initialization code. 
-	* Create global objects here.
-	*/
-	
+	 * Create global objects here.
+	 */
+
 	public void robotInit() {
+
+		frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+
+		// the camera name (ex "cam0") can be found through the roborio web interface
+		session = NIVision.IMAQdxOpenCamera("cam0",
+				NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+		NIVision.IMAQdxConfigureGrab(session);
+
 		talon1 = new Talon(0);
 		talon2 = new Talon(1);
-		
+
 		//reversing 1,2 and 3,4 will switch front and back in arcade mode.
 		driveTrain1 = new RobotDrive(talon1, talon2);
 
@@ -47,17 +66,37 @@ public class Robot extends IterativeRobot {
 	}
 
 	/* This function is called periodically during operator control.
-	* Called at 50Hz (every 20ms). This method must not take more than 20ms to complete! 
-	*/
+	 * Called at 50Hz (every 20ms). This method must not take more than 20ms to complete! 
+	 */
 	public void teleopPeriodic() {
-		
-		normalDrive();
+		NIVision.IMAQdxStartAcquisition(session);
 
-		//check for button press to switch mode. Use two buttons to prevent bounce.
-		boolean button2 = leftStick.getRawButton(2);
-		boolean button3 = leftStick.getRawButton(3);
-		if (button2) driveState = ARCADE;
-		if (button3) driveState = TANK;
+		/**
+		 * grab an image, draw the circle, and provide it for the camera server
+		 * which will in turn send it to the dashboard.
+		 */
+		NIVision.Rect rect = new NIVision.Rect(10, 10, 100, 100);
+
+		while (isOperatorControl() && isEnabled()) {
+
+			NIVision.IMAQdxGrab(session, frame, 1);
+			NIVision.imaqDrawShapeOnImage(frame, frame, rect,
+					DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
+
+			CameraServer.getInstance().setImage(frame);
+
+			normalDrive();
+
+			//check for button press to switch mode. Use two buttons to prevent bounce.
+			boolean button2 = leftStick.getRawButton(2);
+			boolean button3 = leftStick.getRawButton(3);
+			if (button2) driveState = ARCADE;
+			if (button3) driveState = TANK;
+
+			Timer.delay(0.005);		// wait for a motor update time
+		}
+		NIVision.IMAQdxStopAcquisition(session);
+
 	}
 
 	// Drive the robot normally
@@ -69,7 +108,7 @@ public class Robot extends IterativeRobot {
 		}
 	}
 
-  /* This function is called periodically during autonomous */
+	/* This function is called periodically during autonomous */
 	public void autonomousPeriodic() { }
 
 	/* This function is called periodically during test mode */
