0fcfc5a782fcdc72db821d676b59a0afdfd40222
salamander2 <mk_gecko@yahoo.com>
2015-02-03 20:35:25 -0500

    Added results of testing motor driving directions.
    Note that I did not have setInvertedMotors -- because I was not using the Joysticks. It still seemed to work!

--- src/org/raiderrobotics/Robot.java
+++ src/org/raiderrobotics/Robot.java
@@ -1,17 +1,16 @@
 package org.raiderrobotics;
 
-//import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.*;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
+/****
+TEST OF MOTOR DRIVING DIRECTION
 
-//TEST OF MOTOR DRIVING DIRECTION 
+RESULTS: 
+>>> buttons 2, 4, 5: all drove forwards
+>>> setting the Talons directly made the right hand one go backwards
+>>> The drive() function was by far the fastest!!
+
+***/ 
 public class Robot extends IterativeRobot {
   
 	//create object references
@@ -19,45 +18,28 @@ public class Robot extends IterativeRobot {
 	public RobotDrive driveTrain1;
 	Talon talon1, talon2;
 
-
-	/*This function is run when the robot is first started up and should be used for any initialization code. 
-	* Create global objects here.
-	*/
-	
 	public void robotInit() {
-		talon1 = new Talon(0);
-		talon2 = new Talon(1);
-		
-		//reversing 1,2 and 3,4 will switch front and back in arcade mode.
-		driveTrain1 = new RobotDrive(talon1, talon2);
-
+		talon1 = new Talon(1);
+		talon2 = new Talon(2);
 		leftStick = new Joystick(0);
+		driveTrain1 = new RobotDrive(talon1, talon2);
 	}
 
-	/* This function is called periodically during operator control.
-	* Called at 50Hz (every 20ms). This method must not take more than 20ms to complete! 
-	*/
 	public void teleopPeriodic() {
 		
-		if (leftStick.getRawButton(2))  driveTrain1.arcadeDrive(0.5, 0);
-		else if (leftStick.getRawButton(3))  {
-			talon1.set(0.5);
+		if (leftStick.getRawButton(2)) {
+			driveTrain1.arcadeDrive(0.5, 0);
+		} else if (leftStick.getRawButton(3))  {
+			talon1.set(0.5);	//set talon speeds directly
 			talon2.set(0.5);
+		} else if (leftStick.getRawButton(4)) {
+			driveTrain1.drive(0.5, 0);
+		} else if (leftStick.getRawButton(5)) {
+			driveTrain1.tankDrive(0.5, 0.5);
+		} else { //stop motors
+			talon1.set(0.0);
+			talon2.set(0.0);
 		}
-		else if (leftStick.getRawButton(4))  driveTrain1.drive(0.5, 0);
-		else if (leftStick.getRawButton(5))  driveTrain1.tankDrive(0.5, 0.5);
-		else {
-			talon1.set(0.5);
-			talon2.set(0.5);
-		}
-
-
 	}
-
-  /* This function is called periodically during autonomous */
-	public void autonomousPeriodic() { }
-
-	/* This function is called periodically during test mode */
-	public void testPeriodic() { }
 
 }
